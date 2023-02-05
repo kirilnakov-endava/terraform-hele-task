6a76faeaa54f0aba80dbd22008bbcbb7ee465220
Kiril Nakov Endava <102593999+kirilnakov-endava@users.noreply.github.com>
2023-02-05 06:33:41 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "kn-terraform-state-bucket-1"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
+    bucket         = "kn-terraform-state-bucket-1"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
     dynamodb_table = "kn-terraform-state-lock"
   }
 }
